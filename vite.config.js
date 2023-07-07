8bfe49fe1ff3acc760f4f04fc4698ad62bfd8f1d
rafipopz98 <itsrealrafi@gmail.com>
2023-07-07 20:59:34 +0530

    marquee error

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import reactFastMarquee from 'react-fast-marquee';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [reactFastMarquee],
-    },
-  },
 })
